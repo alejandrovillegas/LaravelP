454d6656b78c2631e972d7b710668c5703b895d4
neopilatos <neopilatos@gmail.com>
2014-11-04 04:25:08 +0000

    DB created with artisan

---
+++ app/database/migrations/2014_11_04_040756_create_canditates_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCanditatesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('canditates', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('website_url');
+            $table->text('description');
+            $table->enum('job_type',['full','partial','freelance']);
+            $table->integer('category_id')->unsinged();
+            $table->boolean('avaliable');
+            $table->string('slug');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('canditates');
+	}
+
+}
